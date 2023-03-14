30c3db3ad94f79d37adc8648e10f89a09fc14eb6
MPyragas <122285101+MPyragas@users.noreply.github.com>
2023-03-14 17:16:29 +0200

    minor changes

--- faker/faker.js
+++ faker/faker.js
@@ -9,19 +9,20 @@ function createRandomUser() {
       lastName: faker.name.lastName(),
       email: faker.internet.email(),
       password: faker.internet.password(),
-      birthdate: faker.date.birthdate().toLocaleDateString(),
+      birthdate: faker.date.birthdate().toLocaleDateString('lt-LT'),
     };
   }
 
 const rl = readline.createInterface({input:process.stdin,output:process.stdout});
-const login = await rl.question("Enter username");
-const pass = await rl.question("Enter password");
+const login = await rl.question("Enter username\n");
+const pass = await rl.question("Enter password\n");
 if(login !== "admin"||pass !== "1234") {
     rl.close();
     console.log(chalk.red("Neteisingi prisijungimo duomenys"));
 }else {
 const user = createRandomUser();
-const userStr = `${user.firstName},${user.lastName},${user.email},${user.password},${user.birthdate}\r\n`
+const userStr = `${user.firstName},${user.lastName},${user.email},${user.password},${user.birthdate}\n`
 appendFile("./people.txt",userStr);
 console.log(chalk.green("Duomenys faile sėkmingai išsaugoti"));
 };
+rl.close();
